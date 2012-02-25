83fdca24b5030b6e5a97611cd872ad0faba31bdf
Arvid Kahl <arvidkahl@PPW-PTI-02N.local>
2012-02-25 16:33:10 +0100

    Initial commit via nodester-cli

---
+++ server.js
@@ -0,0 +1,5 @@
+var http = require('http');
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\nApp (spark) is running..');
+}).listen(14904);
