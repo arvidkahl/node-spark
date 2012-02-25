dcdfd99da1f2df30e5420c4368bf995bac748758
Arvid Kahl <mail@arvidkahl.de>
2012-02-25 18:18:04 +0100

    rerouting from script

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\nApp (spark) is running..');
-}).listen(14904);
+require("./main.js");
+
+// listen(14904);
